9633fd054c486be1fbe261c51b71b43dc9f31d35
SohylaIsmael <145512074+SohylaIsmael@users.noreply.github.com>
2023-10-13 16:00:09 +0300

    Update test2.java

--- test2.java
+++ test2.java
@@ -10,23 +10,24 @@ Scanner put= new Scanner(System.in);
 
 System.out.print("Enter num1: "); 
 int num1=in.nextInt();
-System.out.print("/nEnter num2: "); 
+System.out.print("Enter num2: "); 
 int num2=put.nextInt();
 
 try{
-    System.out.println(calc(num1, num2));
+    System.out.println(calc1(num1, num2));
 }catch(ArithmeticException e){
-    System.out.println("Invalid Denominator");}
+    System.out.println("Invalid Denominator");
+    System.out.println(calc2(num1, num2));}
 }
 
-static int calc(int num1, int num2) {
+static int calc1(int num1, int num2) {
     int result;
-    if (num2 != 0)
        return result= num1/num2;
+  }
 
-    else {
-        return result= num1+num2;
-    }
+static int calc2(int num1, int num2) {
+    int result;
+       return result= num1+num2;
   }
 
 }
